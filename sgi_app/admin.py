1689357ea3a10d537c7b0db22be25fcc79395b04
Jhefrey <jhefreyzerdna@gmail.com>
2024-02-20 15:03:19 -0500

    Archivo serializers.py

--- sgi_app/admin.py
+++ sgi_app/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Producto
-from .models import Proveedor
-from .models import Pedido
+from .models import Proveedor, Producto, Pedido
 
 # Register your models here.
 
