5557848e3ff3a19223e50809159fbc2ef699c019
Jhefrey <jhefreyzerdna@gmail.com>
2024-02-20 14:23:41 -0500

    Definición de modelos para sgi_app

--- sgi_app/models.py
+++ sgi_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    cantidad_disponible = models.PositiveIntegerField(default=0)
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
+
+class Pedido(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
+    fecha_pedido = models.DateField(auto_now_add=True)
+    fecha_entrega = models.DateField()
+
+    def __str__(self):
+        return f"Pedido de {self.producto.nombre} ({self.cantidad} unidades)"
+
