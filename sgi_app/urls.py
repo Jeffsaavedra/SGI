e70dddfde58c73bc7ca7844b619dd4328d454437
Jhefrey <jhefreyzerdna@gmail.com>
2024-02-20 23:00:29 -0500

    vistas CRUD y URLs

--- sgi_app/urls.py
+++ sgi_app/urls.py
@@ -1,10 +1,28 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import ProveedorListCreate, ProductoListCreate, PedidoListCreate
+from .views import (ProveedorListCreate, ProductoListCreate, 
+                    PedidoListCreate, ProductoDetail, 
+                    ProductoRetrieveUpdateDestroy, PedidoDetail, 
+                    PedidoRetrieveUpdateDestroy, ProveedorDetail, 
+                    ProveedorRetrieveUpdateDestroy)
 
 urlpatterns = [
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # Para obtener tokens
+    # URLS APIS PROVEEDORES
     path('api/proveedores/', ProveedorListCreate.as_view(), name='proveedor-list-create'),
+    path('api/proveedores/<int:pk>/', ProveedorDetail.as_view(), name='proveedor-detail'),
+    path('api/proveedores/<int:pk>/update/', ProveedorRetrieveUpdateDestroy.as_view(), name='proveedor-update'),
+    path('api/proveedores/<int:pk>/delete/', ProveedorRetrieveUpdateDestroy.as_view(), name='proveedor-delete'),
+    # URLS APIS PRODUCTOS 
     path('api/productos/', ProductoListCreate.as_view(), name='producto-list-create'),
+    path('api/productos/<int:pk>/', ProductoDetail.as_view(), name='producto-detail'),
+    path('api/productos/<int:pk>/update/', ProductoRetrieveUpdateDestroy.as_view(), name='producto-update'),
+    path('api/productos/<int:pk>/delete/', ProductoRetrieveUpdateDestroy.as_view(), name='producto-delete'),
+    # URLS APIS PEDIDOS 
     path('api/pedidos/', PedidoListCreate.as_view(), name='pedido-list-create'),
+    path('api/pedidos/<int:pk>/', PedidoDetail.as_view(), name='pedido-detail'),
+    path('api/pedidos/<int:pk>/update/', PedidoRetrieveUpdateDestroy.as_view(), name='pedido-update'),
+    path('api/pedidos/<int:pk>/delete/', PedidoRetrieveUpdateDestroy.as_view(), name='pedido-delete'),
 ]
+
+
--- sgi_app/views.py
+++ sgi_app/views.py
@@ -5,18 +5,122 @@ from .models import Proveedor, Producto, Pedido
 from .serializers import ProveedorSerializer, ProductoSerializer, PedidoSerializer
 #from .permissions import IsAdminOrReadOnly
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import status
+from django.http import Http404
 
+#------------------------------->>>>>>>>>>>>VISTAS PROVEEDOR<<<<<<<<<-----------------------------------    
+# Vista para crear proveedor
 class ProveedorListCreate(generics.ListCreateAPIView):
     queryset = Proveedor.objects.all()
     serializer_class = ProveedorSerializer
     permission_classes = [IsAuthenticated]
     
+#Vista para  actualizar y eliminar proveedor
+class ProveedorRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Proveedor.objects.all()
+    serializer_class = ProveedorSerializer
+
+#vista detalle para proveedor
+class ProveedorDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Proveedor.objects.get(pk=pk)
+        except Proveedor.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        proveedor = self.get_object(pk)
+        serializer = ProveedorSerializer(proveedor)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        proveedor = self.get_object(pk)
+        serializer = ProveedorSerializer(proveedor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        proveedor = self.get_object(pk)
+        proveedor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+#------------------------------->>>>>>>>>>>>VISTAS PRODUCTO<<<<<<<<<-----------------------------------    
+# Vista para crear producto
 class ProductoListCreate(generics.ListCreateAPIView):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
     permission_classes = [IsAuthenticated]
 
+#Vista para  actualizar y eliminar producto
+class ProductoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+
+#vista detalle para producto 
+class ProductoDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Producto.objects.get(pk=pk)
+        except Producto.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        producto = self.get_object(pk)
+        serializer = ProductoSerializer(producto)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        producto = self.get_object(pk)
+        serializer = ProductoSerializer(producto, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        producto = self.get_object(pk)
+        producto.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+#------------------------------->>>>>>>>>>>>VISTAS PEDIDO<<<<<<<<<-----------------------------------    
+# Vista para crear pedido
 class PedidoListCreate(generics.ListCreateAPIView):
     queryset = Pedido.objects.all()
     serializer_class = PedidoSerializer
     permission_classes = [IsAuthenticated]
+
+#Vista para  actualizar y eliminar
+class PedidoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
+
+#vista detalle para pedido
+class PedidoDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Pedido.objects.get(pk=pk)
+        except Pedido.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        pedido = self.get_object(pk)
+        serializer = PedidoSerializer(pedido)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        pedido = self.get_object(pk)
+        serializer = PedidoSerializer(pedido, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        pedido = self.get_object(pk)
+        pedido.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
